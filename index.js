8ea4b73be762e040ccb6d1530d7c38acecfc3110
Kennedy-oss <kennedy.onyango4@gmail.com>
2023-10-16 15:00:28 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+// Code your solution in this file!
+function receivesAFunction(){
+
+}
+function receivesAFunction(){
+
+}
+
+function receivesAFunction(spy){
+spy()
+}
+function returnsANamedFunction() {
+    return function namedFunction() {
+      // function body
+    };
+  }
+
+  function returnsAnAnonymousFunction() {
+    return function() {
+      // function body
+    };
+  }
